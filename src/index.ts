df6b577e068b58142466c30e7598061e1d5a95ca
kross <cristemperatum@gmail.com>
2022-01-10 11:53:54 -0600

    hacker rank9

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+// Source: https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+function timeConversion(s: string) : string {
+  
+  let lastTwo = s.substring(8);
+  //para ignorar el AM y PM 
+  let fullTime = s.substring(0, 8);
+  let times = fullTime.split(':');
+  let time = 0;
+  if(lastTwo === "PM") {
+    if (times[0] !== "12"){
+      time = parseInt(times[0], 10) + 12 ;
+      times[0] = time.toString(); 
+    }
+  } else {
+    times[0] 
+    //para el caso AM
+    if (times[0] === "12") {
+          times[0] = "00"; 
+    }
+  }
 
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
+  return times.join(':');
 }
-
+console.log('---------- Cool Programing ----------\n');
 function main() {
-  console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const testString = '07:05:45PM' ;
+  const result = timeConversion(testString);
   console.log('Result: ', result);
 }
-
 main();
